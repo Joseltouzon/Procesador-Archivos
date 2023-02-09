c79fee1eb4881021d39402ce87b74e2548bf3961
Jose Luis Lopez Touzon <josel.touzon@gmail.com>
2023-02-09 18:52:11 -0300

    procesadores finalizados

--- index.php
+++ index.php
@@ -17,3 +17,13 @@
         <input type="submit" value="Procesar Archivo">
     </form>
 </div>
+<br>
+<div>
+    <h3>Procesador de archivos Rend.Rev</h3>
+    <h6>Solo acepta formato .txt</h6>
+    <form action="procesa_archivos.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file" accept=".txt" require>
+        <br><br>
+        <input type="submit" value="Procesar Archivo">
+    </form>
+</div>
---
+++ procesa_archivos.php
@@ -0,0 +1,67 @@
+<?php
+
+$file = $_FILES['file']['name'];
+
+$destino = 'archivos/' . $file;
+
+move_uploaded_file($_FILES['file']['tmp_name'], $destino);
+
+$fileContenido = file_get_contents($destino);
+$fileRows = explode("\n", $fileContenido);
+$totalTrans = count($fileRows);
+
+
+?>
+
+<div style="float: left;">
+
+    <h2 style="text-align: center;">Resultados</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th style="text-align: center;"><b>Identificador del Cliente</b></th>
+                <th style="text-align: center;"><b>Monto</b></th>
+                <th style="text-align: center;"><b>Fecha de Pago</b></th>
+                <th style="text-align: center;"><b>Medio de Pago</b></th>
+
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            $archivo = fopen($destino, 'r');
+            while (!feof($archivo)) {
+                $linea = fgets($archivo);
+                $saltolinea = nl2br($linea);
+
+                $colum1 = substr($saltolinea, 13, 6);
+                $colum2 = substr($saltolinea, 77, -269);
+                $colum3 = substr($saltolinea, 89, -261);
+                $colum4 = substr($saltolinea, 284, -65);
+
+                $totalMonto += $colum2;
+            ?>
+                <tr>
+                    <td style="text-align: center;"><?= $colum1 ?></td>
+                    <td style="text-align: center;"><?= $colum2 ?></td>
+                    <td style="text-align: center;"><?= $colum3 ?></td>
+                    <td style="text-align: center;"><?= $colum4 ?></td>
+
+                </tr>
+
+            <?php
+            }
+
+            fclose($archivo);
+            ?>
+        </tbody>
+    </table>
+</div>
+<div style="float: right;">
+    <h2 style="text-align: center;">Totales</h2>
+    <br>
+    <h4 style="text-align: center;">Total Transacciones: <?= $totalTrans ?></h4>
+    <br>
+    <h4 style="text-align: center;">Monto Total Cobrado: <?= $totalMonto ?></h4>
+</div>
